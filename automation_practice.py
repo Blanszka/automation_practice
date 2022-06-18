b6f1f2dc84cb4cf4ce70fbccf94dbdc44c9ec00a
Blanszka <monika.porydzajj@gmail.com>
2022-06-18 16:22:09 +0200

    first commit

--- automation_practice.py
+++ automation_practice.py
@@ -1,14 +1,14 @@
-#BIBLIOTEKI
+#Libraries
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 import time
 
-#DANE
-ilosc = '2' # ilość jaką dodajemy do koszyka
-rozmiar = "M" # S / M / L
-kolor = "Orange" # Blue / Orange / Black / Yellow
+#data
+amount = '2' # amount in the cart
+size_dress = "M" # S / M / L
+colour_dress = "Orange" # Blue / Orange / Black / Yellow
 
 
 #setUp
@@ -18,51 +18,51 @@ driver.maximize_window()
 driver.get("http://automationpractice.com/index.php")
 driver.implicitly_wait(20)
 
-#wyszukanie sukienki
-wyszukaj = driver.find_element(By.NAME, "search_query")
-wyszukaj.click()
-wyszukaj.send_keys("dress")
-wyszukaj.send_keys(Keys.ENTER)
+#search "dress"
+search_dress = driver.find_element(By.NAME, "search_query")
+search_dress.click()
+search_dress.send_keys("dress")
+search_dress.send_keys(Keys.ENTER)
 
-#sortowanie listy
-sortowanie = \
+#list sorting #in stock
+sort = \
 driver.find_element(By.ID, 'selectProductSort')
-select_sort = Select(sortowanie)
+select_sort = Select(sort)
 select_sort.select_by_visible_text("In stock")
 time.sleep(5)
 
-#wybiera pirwszy produkt ze strony
+#picks first product from the list
 driver.find_element(By.XPATH, '//*[@id="center_column"]/ul/li[1]/div/div[1]/div/a[1]/img').click()
 
-#ilosc produktu
+#quantity
 quantity = driver.find_element(By. ID, "quantity_wanted")
 quantity.click()
 quantity.send_keys(Keys.BACKSPACE)
-quantity.send_keys(ilosc)
+quantity.send_keys(amount)
 
-#rozmiar produktu
+#size of the dress
 size = Select(driver.find_element(By.ID, "group_1"))
-size.select_by_visible_text(rozmiar)
+size.select_by_visible_text(size_dress)
 
-#kolor produktu
-driver.find_element(By. XPATH, "//a[@name='" + kolor + "']").click()
+#colour of the dress
+driver.find_element(By. XPATH, "//a[@name='" + colour_dress + "']").click()
 
-#dodaj do koszyka
+#add to cart
 driver.find_element(By. ID, "add_to_cart").click()
 driver.find_element(By. XPATH, "//a[@class='btn btn-default button button-medium']").click()
 time.sleep(5)
 
-#zweryfikuj czy w koszyku zgadza się ilość produktów
-koszyk = (driver.find_element(By. XPATH, "//input[@class='cart_quantity_input form-control grey']").get_attribute("value"))
-if koszyk == ilosc:
+#verify amount in the cart
+basket = (driver.find_element(By. XPATH, "//input[@class='cart_quantity_input form-control grey']").get_attribute("value"))
+if basket == amount:
     pass
     print('OK :)')
 else:
-    print('Błąd! :c ')
+    print('Somethings wrong! :c ')
     exit()
 
-#wyczyść koszyk
+#empty your cart
 driver.find_element(By.ID, "5_25_0_0").click()
 
-time.sleep(5)
+time.sleep(2)
 driver.quit()
